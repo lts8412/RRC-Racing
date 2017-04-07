efe988999eb315c5ceacb09e918ca0ee18428bef
lts8412 <000@catlover.com>
2017-04-06 22:06:06 -0500

    Update RRC-Objects.js

--- examples/js/RRC-Objects.js
+++ examples/js/RRC-Objects.js
@@ -12,6 +12,6 @@ function track(sizeX, sizeY, segmentsX, segmentsY, positionX, positionY, positio
 	ground.position.y = positionY;
 	ground.position.z = positionZ;
 	ground.rotation.x = 4.71239;//No need to change
-	ground.rotation.y = 4.71239; //Will Need to be changed to create the banking of the track
+	ground.rotation.y = 0;//4.71239; //Will Need to be changed to create the banking of the track
 	scene.add( ground );
 }
